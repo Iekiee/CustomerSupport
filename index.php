766f601d8ecee2cf81e3d3a5ad33061783d30aad
Igone van_der_Kley <igone@vdkley.nl>
2023-06-09 13:42:54 +0200

    index.php form skeleton added

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Support</title>
+</head>
+<body>
+    <h1>Customer Support</h1>
+
+    <form>
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
+
+        <label for="message">message:</label>
+        <input type="message" id="message" name="message">
+
+
+
+
+
+    </form>
+</body>
+</html>
