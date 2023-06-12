ea1d8cb33f2982be7aff20f6b989a0d55b8506db
Igone van_der_Kley <igone@vdkley.nl>
2023-06-12 17:57:20 +0200

    modified create script

--- script.sql
+++ script.sql
@@ -1,12 +1,13 @@
 drop database if exists CustomerSupport;
+create database CustomerSupport
 use CustomerSupport;
  
 create table CustomerSupportData (
-    Id          INT             NOT NULL AUTO_INCREMENT,
-    Name        VARCHAR(50)    
+    Id          INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY
+    ,Name        VARCHAR(50)    
     ,Email      VARCHAR(300)    NOT NULL
     ,Message    VARCHAR(500)    NOT NULL
-)
+);
 
 
 
