ca0dda922685f38c26ab08dcc12ac528525f1e89
Igone van_der_Kley <igone@vdkley.nl>
2023-06-12 17:57:47 +0200

    conformation page added

---
+++ conformation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>conformation</title>
+</head>
+<body>
+    <h1>Thank you for your message, 
+        we hope to get back to you shortly.
+
+        <a href="index.php">back</a>
+    </h1>
+</body>
+</html>
