61415838b329397a089eb792b3baefe6f2ac4197
Igone van_der_Kley <igone@vdkley.nl>
2023-06-12 17:58:42 +0200

    header & footer

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <div class="headerContent">
+        <ul>
+            <li> <a href="#">WordOne</a></li>
+            <li> <a href="#">WordTwo</a></li>
+            <li> <a href="#">WordThree</a></li>
+        </ul>
+    </div>
+</body>
+</html>
